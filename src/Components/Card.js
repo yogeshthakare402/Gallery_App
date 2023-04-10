85d164966cc041ec8ba39ba2a25a6edd68d21185
yogeshthakare402 <yogeshthakare402@gmail.com>
2023-04-10 19:51:35 +0530

    set id for delete

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 
 const Card = forwardRef(({ imageName,url,id,suretoDelete,setSureToDelete, password},ref) => {
     const [showDelete, setShowDelete] = useState(false);
-    const [deleteId,setDeleteId] = useState('')
+    // const [deleteId,setDeleteId] = useState('')
 
     useImperativeHandle(ref,()=>({
          deleteImage() {
             console.log(id)
+            // console.log(deleteId)
             // let url = `http://localhost:8000/api/gallery/images/:${deleteId}`;
-            let url = `https://gallery-api-rpqq.onrender.com/api/gallery/images/:${deleteId}`;
+            let url = `https://gallery-api-rpqq.onrender.com/api/gallery/images/:${id}`;
             console.log(password)
             if(suretoDelete && password ==='yogesh'){
                 axios.delete(url)
@@ -34,7 +35,7 @@ const Card = forwardRef(({ imageName,url,id,suretoDelete,setSureToDelete, passwo
         <div className='card' onMouseEnter={() => { setShowDelete(true) }} onMouseLeave={() => { setShowDelete(false) }}>
             {showDelete && <button className='delete' onClick={() =>{
                 setSureToDelete(true)
-                setDeleteId(id)}}>Delete</button>}
+                }}>Delete</button>}
             <img src={url} alt={imageName} />
             <div className='imageName'>{imageName}</div>
         </div>
